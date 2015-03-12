212b62d0cb13e1ece1b89d9328ede74e99f5adc8
root <root@VM001383.localdomain>
2015-03-12 12:46:23 +0900

    fix address in pictures table

--- script/Iwate/registerAlbumsToDBfromCSV.rb
+++ script/Iwate/registerAlbumsToDBfromCSV.rb
@@ -6,19 +6,20 @@ applicationID = 2 # => 岩手県アプリで利用することを意味する
 db_name = "../../db/production.sqlite3"
 db = SQLite3::Database.new(db_name)
 
-post_id = db.execute("select * from albums").size + 1
+post_id = db.execute("select * from albumall").size + 1
 
-open("finalWithShi.csv") do |file|
+#open("finalWithShi.csv") do |file|
+open("AlbumData.csv") do |file|
   while l = file.gets
     array = l.chop.split(",")
     title = array[0]
-    album_id = array[4]
+    album_id = array[1]
     latitude = array[2]
     longitude = array[3]
-    address = array[1]
-    area = array[5]
-    description = array[6]
-    detail = array[7]
+    address = array[5]
+    area = ""#array[5]
+    description = ""#array[6]
+    detail = ""#array[7]
     representative_picture = 1
 
   next if area == nil
@@ -36,11 +37,11 @@ open("finalWithShi.csv") do |file|
     puts description
     puts detail
     puts "----------"
-  line = title + "," + album_id + "," + latitude + "," + longitude +"," + address + "," + area + "," +description + "," + detail
+#  line = title + "," + album_id + "," + latitude + "," + longitude +"," + address + "," + area + "," +description + "," + detail
 
     #Albumsテーブルへデータ保存
     db.transaction do
-      sql = "insert into albums values(?,?,?,?,?,?,?,?,?,?,?,?,?)"
+      sql = "insert into albumall values(?,?,?,?,?,?,?,?,?,?,?,?,?)"
       db.execute(sql,post_id.to_i,title,address,latitude.to_f,longitude.to_f,nil,nil,representative_picture,album_id,area,applicationID,description,detail)
       post_id = post_id.to_i + 1
     end
